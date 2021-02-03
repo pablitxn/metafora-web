348cf4a1c0fc39f1a33feb4c5954ca64d4996349
Pablo Coronel <pablo.coronel@wearepsh.com>
2021-02-03 17:48:30 -0300

    fix(styles): responsive styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
-// import AppLayout from "layouts/app";
-import Footer from "components/_shared/footer";
 import "./styles.less";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-	return (
-		<>
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
 import { FC } from "react";
 import Image from "next/image";
 import "./styles.less";
+import Footer from "components/_shared/footer";
 
 const Home: FC = () => {
 	return (
-		<div className="web">
-			<img
-				src="/logos/transparent_color.png"
-				alt="Picture of the author"
-				className="web__img"
-			/>
-			<article className="web__article">
-				<h1>Prepárense ... Algo realmente genial está llegando pronto</h1>
-				<p>
-					Estamos trabajando en nuestra Web. ¡Los esperamos en febrero! Nos
-					podes contactar a info@metafora.org.ar o en nuestras redes sociales
-				</p>
-			</article>
+		<div className="home">
+			<div className="home__content">
+				<img
+					alt="Picture of the author"
+					src="/logos/transparent_color-full-size.png"
+					className="home__img"
+				/>
+				<article className="home__article">
+					<h1>Prepárense ... Algo realmente genial está llegando pronto</h1>
+					<p>
+						Estamos trabajando en nuestra Web. ¡Los esperamos en febrero! Nos
+						podes contactar a info@metafora.org.ar o en nuestras redes sociales
+					</p>
+				</article>
+			</div>
+			<Footer />
 		</div>
 	);
 };
