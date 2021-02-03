c58c1bd17eaa92adaf10e8a67401da6bb61f789c
Pablo Coronel <pablo.coronel@wearepsh.com>
2021-02-02 22:11:32 -0300

    feat(app): se creo la pagina temporal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
-import AppLayout from "layouts/app";
+// import AppLayout from "layouts/app";
+import Footer from "components/_shared/footer";
 import "./styles.less";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
 	return (
-		<AppLayout>
+		<>
 			<Component {...pageProps} />
-		</AppLayout>
+			<Footer />
+		</>
 	);
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import { FC } from "react";
-import HomeLayout from "layouts/home";
+import Image from "next/image";
+import "./styles.less";
 
 const Home: FC = () => {
-	return <HomeLayout />;
+	return (
+		<div className="web">
+			<img
+				src="/logos/transparent_color.png"
+				alt="Picture of the author"
+				className="web__img"
+			/>
+			<article className="web__article">
+				<h1>Prepárense ... Algo realmente genial está llegando pronto</h1>
+				<p>
+					Estamos trabajando en nuestra Web. ¡Los esperamos en febrero! Nos
+					podes contactar a info@metafora.org.ar o en nuestras redes sociales
+				</p>
+			</article>
+		</div>
+	);
 };
 
 export default Home;
