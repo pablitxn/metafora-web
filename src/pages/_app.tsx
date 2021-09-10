b1e43726aa8eada4f4cf3ee2e82b6b1f1b6363de
Ignacio Zamboni <ignacio.zamboni20@gmail.com>
2021-09-10 17:13:17 -0300

    fix(app): remove-ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,17 @@ import React, { FunctionComponent } from 'react';
 import { AppProps } from 'next/app';
 import './globals.css';
 
+const SafeHydrate: FunctionComponent = ({ children }) => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return <SafeHydrate><Component {...pageProps} /></SafeHydrate>;
 };
 
 export default App;
