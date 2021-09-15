e19bf179a464d09a8c9942baae6a8041de8adbde
Ignacio Zamboni <ignacio.zamboni20@gmail.com>
2021-09-15 16:41:45 -0300

    wip: header

--- src/components/orquestando-futuros/header/index.tsx
+++ src/components/orquestando-futuros/header/index.tsx
@@ -1,12 +1,27 @@
 import { FunctionComponent } from 'react';
-import { HeaderContent, Logo, Links } from './styles';
+import { Instagram, Mail } from 'react-feather';
+import Navigation from 'components/_shared/navigation-anchor';
+import {
+  HeaderContainer,
+  Logo,
+  NavigationContainer,
+  Divider,
+  SocialMediaContainer,
+} from './styles';
 
 const Header: FunctionComponent = () => {
   return (
-    <HeaderContent>
-      <Logo src="/images/logo-orquestando-1.png" alt="logo" />
-      <Links />
-    </HeaderContent>
+    <HeaderContainer>
+      <Logo src="/logos/orquestando-1.png" alt="logo" />
+      <NavigationContainer>
+        <Navigation href="/">Fundacion Metafora</Navigation>
+        <Divider />
+        <SocialMediaContainer>
+          <Instagram />
+          <Mail />
+        </SocialMediaContainer>
+      </NavigationContainer>
+    </HeaderContainer>
   );
 };
 
--- src/components/orquestando-futuros/header/styles.ts
+++ src/components/orquestando-futuros/header/styles.ts
@@ -1,13 +1,53 @@
 import styled from 'styled-components';
 
-export const HeaderContent = styled.div`
-  /* position: absolute; */
-  /* top: 0; */
+export const HeaderContainer = styled.div`
   width: 100%;
   z-index: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  /* padding: 0 3rem; */
+  width: 100%;
+  padding: 3%;
 `;
+
 export const Logo = styled.img`
-  margin-top: 45px;
-  margin-left: 109px;
+  /* margin-top: 2.8125rem; */
+  /* margin-left: 6.8125rem; */
+  margin-left: 4%;
 `;
-export const Links = styled.div``;
+
+export const NavigationContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  gap: 2.5rem;
+  color: #feffdf;
+  margin-right: 7%;
+  /* margin-top: 2%; */
+  a {
+    pointer-events: none;
+    font-weight: 500;
+    text-decoration: none;
+    color: #feffdf;
+    font-size: 1.1rem;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 2px;
+  height: 1.5rem;
+  background-color: #feffdf;
+`;
+
+export const SocialMediaContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+
+  svg {
+    cursor: pointer;
+  }
+`;
