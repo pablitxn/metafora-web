010f165cf5038ecebfca0c29258cc5d0a50bb242
Pablo Coronel <me@pablitxn.io>
2021-10-02 16:48:41 -0300

    Revert "fix(footer): text typo"
    This reverts commit 870a9a8c8138bc46c0b3317a610150fe6efe2bc5.

---
+++ src/components/_shared/footer/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import "./styles.less";
+
+const Footer: FC = () => {
+	return <footer className="footer">contacto@metafora.org.ar</footer>;
+};
+
+export default Footer;
